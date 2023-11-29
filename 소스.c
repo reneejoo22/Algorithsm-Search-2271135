00162cb9f6c471dc4018edc3162b846877527b5e
reneejoo22 <renee.joo03@gmail.com>
2023-11-30 02:12:52 +0900

    정답

--- 소스.c
+++ 소스.c
@@ -1,108 +1,125 @@
-//다양한 탐색 방법
-
-#include<stdio.h>
-#include <stdlib.h>
+#include<stdio.h> 
+#include<stdlib.h> 
 
 #define MAX(a, b) (a)
-
-typedef struct AVLNode {
+// AVL 트리 노드 정의
+typedef struct AVLNode
+{
 	int key;
 	struct AVLNode* left;
 	struct AVLNode* right;
-}AVLNode;
+} AVLNode;
 
-int get_height(AVLNode* node) {
-	
+// 트리의 높이를 반환
+int get_height(AVLNode* node)
+{
 	int height = 0;
-	
+
 	if (node != NULL)
-		height = 1 + MAX(get_height(node->left), get_height(node->right));
-			
+		height = 1 + max(get_height(node->left),
+			get_height(node->right));
+
 	return height;
 }
+// 노드의 균형인수를 반환
+int get_balance(AVLNode* node)
+{
+	if (node == NULL) return 0;
 
-int get_balance(AVLNode* node) {	//균형인수 = 왼노드갯수 - 른노드갯수
-
-	if (node == NULL)return 0;
-
-	return get_height(node->left) - get_height(node->right);
+	return get_height(node->left)
+		- get_height(node->right);
 }
 
-AVLNode* create_node(int key) {
-
+// 노드를 동적으로 생성하는 함수
+AVLNode* create_node(int key)
+{
 	AVLNode* node = (AVLNode*)malloc(sizeof(AVLNode));
-
 	node->key = key;
 	node->left = NULL;
 	node->right = NULL;
 	return(node);
 }
 
-AVLNode* rotate_right(AVLNode* parent) {	// LL 삽입 경우
-
+// 오른쪽으로 회전시키는 함수
+AVLNode* rotate_right(AVLNode* parent)
+{
 	AVLNode* child = parent->left;
 	parent->left = child->right;
 	child->right = parent;
 
+	// 새로운 루트를 반환
 	return child;
 }
 
-AVLNode* rotate_left(AVLNode* parent) {	// RR 삽입 경우
-
+// 왼쪽으로 회전시키는 함수
+AVLNode* rotate_left(AVLNode* parent)
+{
 	AVLNode* child = parent->right;
 	parent->right = child->left;
 	child->left = parent;
 
+	// 새로운 루트 반환
 	return child;
 }
 
-AVLNode* insert(AVLNode* node, int key) {
-
+// AVL 트리에 새로운 노드 추가 함수
+// 새로운 루트를 반환한다. 
+AVLNode* insert(AVLNode* node, int key)
+{
+	// 이진 탐색 트리의 노드 추가 수행
 	if (node == NULL)
 		return(create_node(key));
-	
-	if (key < node->key)	//키가 작으면
+
+	if (key < node->key)
 		node->left = insert(node->left, key);
 	else if (key > node->key)
 		node->right = insert(node->right, key);
-	else
+	else	// 동일한 키는 허용되지 않음
 		return node;
 
+	// 노드들의 균형인수 재계산
 	int balance = get_balance(node);
 
-	//LL 타입
+	// LL 타입 처리
 	if (balance > 1 && key < node->left->key)
 		return rotate_right(node);
-	
-	//RR 타입
+
+	// RR 타입 처리
 	if (balance < -1 && key > node->right->key)
 		return rotate_left(node);
-	
-	//LR 타입
-	if (balance > 1 && key > node->left->key) {
+
+	// LR 타입 처리
+	if (balance > 1 && key > node->left->key)
+	{
 		node->left = rotate_right(node->left);
 		return rotate_right(node);
 	}
-	//RL 타입
-	if (balance < -1 && key < node->right->key) {
+
+	// RL 타입 처리
+	if (balance < -1 && key < node->right->key)
+	{
 		node->right = rotate_right(node->right);
 		return rotate_left(node);
 	}
 	return node;
 }
 
-void preorder(AVLNode* root) {
-	if (root != NULL) {
+// 전위 순회 함수
+void preorder(AVLNode* root)
+{
+	if (root != NULL)
+	{
 		printf("[%d] ", root->key);
 		preorder(root->left);
 		preorder(root->right);
 	}
 }
 
-int main(void) {
-
+int main(void)
+{
 	AVLNode* root = NULL;
 
+	// 예제 트리 구축
 	root = insert(root, 10);
 	root = insert(root, 20);
 	root = insert(root, 30);
@@ -110,7 +127,7 @@ int main(void) {
 	root = insert(root, 50);
 	root = insert(root, 29);
 
-	printf("전위 순회 결과\n");
+	printf("전위 순회 결과 \n");
 	preorder(root);
 
 	return 0;
